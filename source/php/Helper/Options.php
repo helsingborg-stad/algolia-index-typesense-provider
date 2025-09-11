55e75b152031e175ed705eb4f257a444f3f46436
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2025-09-11 13:10:14 +0200

    fallback to auto generated collection name

--- source/php/Helper/Options.php
+++ source/php/Helper/Options.php
@@ -31,12 +31,12 @@ class Options
         return get_field('algolia_index_typesense_public_api_key', 'option') ?: null;
     }
 
-    public static function collectionName(): ?string
+    public static function collectionName(): string
     {
         if (defined('TYPESENSEINDEX_COLLECTION_NAME') && !empty(TYPESENSEINDEX_COLLECTION_NAME)) {
             return TYPESENSEINDEX_COLLECTION_NAME;
         }
 
-        return get_field('algolia_index_typesense_collection_name', 'option') ?: null;
+        return get_field('algolia_index_typesense_collection_name', 'option') ?: \AlgoliaIndex\Helper\Options::indexName();
     }
 }
