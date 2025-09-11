9fed58ba6248779380df6e45d5f3f2b6121ac6ad
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2025-09-11 13:10:26 +0200

    use options

--- source/php/Provider/Typesense/TypesenseProviderFactory.php
+++ source/php/Provider/Typesense/TypesenseProviderFactory.php
@@ -2,14 +2,16 @@
 
 namespace AlgoliaIndexTypesenseProvider\Provider\Typesense;
 
+use AlgoliaIndexTypesenseProvider\Helper\Options;
+
 class TypesenseProviderFactory
 {
     public static function createFromEnv()
     {
         return new TypesenseProvider(
-            TYPESENSEINDEX_API_KEY, 
-            TYPESENSEINDEX_API_URL, 
-            defined('TYPESENSEINDEX_COLLECTION_NAME') && !empty(TYPESENSEINDEX_COLLECTION_NAME) ? TYPESENSEINDEX_COLLECTION_NAME : \AlgoliaIndex\Helper\Options::indexName(),
+            Options::apiKey(), 
+            Options::apiUrl(), 
+            Options::collectionName()
         );
     }
 }
